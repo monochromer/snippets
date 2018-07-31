8a8d0d0659018c2911dec40ce265b94ba6143e16
monochromer <monochromer@users.noreply.github.com>
2018-07-31 16:02:18 +0500

    Update loadImage.js

--- snippets/loadImage.js
+++ snippets/loadImage.js
@@ -1,27 +1,44 @@
-function withDecode(image, cb) {
-  "decode" in image
-    ? image.decode().then(function() { cb(image) })
-    : cb(image)
-}
-
 function loadImage(image, callback) {
   var isImageElement = (image instanceof HTMLImageElement || image instanceof Image);
   var instance = isImageElement ? image : new Image();
 
-  return new Promise(function (resolve, reject) {
-    instance.onload = function () {
+  return new Promise(function(resolve, reject) {
+    instance.onload = function() {
       instance.onload = instance.onerror = null;
-      typeof callback === 'function' && withDecode(instance, function() { callback(null, instance) });
-      withDecode(instance, resolve);
+      typeof callback === 'function' && callback(null, instance);
+      resolve(instance);
     };
 
-    instance.onerror = function (e) {
+    instance.onerror = function(error) {
       instance.onload = instance.onerror = null;
-      typeof callback === 'function' && callback(e, null);
-      reject(e);
+      typeof callback === 'function' && callback(error, null);
+      reject(error);
     };
 
     if (!isImageElement) instance.src = image;
-    if (instance.complete) withDecode(instance, resolve);
+    if (instance.complete) {
+      instance.onload = instance.onerror = null;
+      typeof callback === 'function' && callback(null, instance);
+      resolve(instance);
+    }
   })
+}
+
+
+function loadImageWithDecode(image, callback) {
+  var isImageElement = (image instanceof HTMLImageElement || image instanceof Image);
+  var instance = isImageElement ? image : new Image();
+
+  if ('decode' in instance) {
+    if (!isImageElement) instance.src = image;
+    return instance.decode()
+      .then(function() {
+        callback(null, instance);
+      })
+      .catch(function(error) {
+        callback(error);
+      })
+  } else {
+    return loadImage(image, callback)
+  }
 }
