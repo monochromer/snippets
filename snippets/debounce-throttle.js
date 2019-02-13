a71751a857f7841f44d5500999d5bf96c2573f74
monochromer <monochromer@users.noreply.github.com>
2019-02-13 21:48:22 +0500

    Update debounce-throttle.js

--- snippets/debounce-throttle.js
+++ snippets/debounce-throttle.js
@@ -29,6 +29,20 @@ function throttle(action) {
   }
 }
 
+// The debounce function receives our function as a parameter
+// https://codepen.io/rikschennink/pen/yZYbwQ?editors=0010
+const debounce = (fn) => {
+  let frame;
+  return (...params) => {
+    if (frame) { 
+      cancelAnimationFrame(frame);
+    }
+    frame = requestAnimationFrame(() => {
+      fn(...params);
+    });
+  } 
+};
+
 
 /**
  * debounce
